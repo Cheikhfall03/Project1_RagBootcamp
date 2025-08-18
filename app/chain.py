a3fe23ed9f3ceeca810b76da40f667898fc78b57
Cheikhfall03 <cheikh.fall@aims-senegal.org>
2025-08-18 22:44:54 +0000

    Update chain.py

--- app/chain.py
+++ app/chain.py
@@ -5,9 +5,10 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from app.vectorstore_simple import store_chunks, get_vectorstore, get_bm25_retriever
 from langchain.retrievers import EnsembleRetriever
-#from langchain_community.cross_encoders import HuggingFaceCrossEncoder
 from sentence_transformers import CrossEncoder
+from langchain.retrievers import ContextualCompressionRetriever
 from langchain.retrievers.document_compressors import CrossEncoderReranker
+from langchain_community.cross_encoders import HuggingFaceCrossEncoder
 from app.config import GROQ_API_KEY
 
 
